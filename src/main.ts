b01589f6f65f6e92fd99afba5fe052fe85763fa0
Anthony Pena <anthony.pena@outlook.fr>
2024-11-13 23:57:41 +0100

    naive async factory provider

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import { Component, inject, Injectable } from "@angular/core";
+import { JsonPipe } from "@angular/common";
+import { Component, inject, Injectable, InjectionToken } from "@angular/core";
 import { bootstrapApplication } from "@angular/platform-browser";
 
 @Injectable()
@@ -6,18 +7,33 @@ export class SyncService {
   config = { appName: "Async provider demo" };
 }
 
+const AsyncConfig = new InjectionToken<{ url: string }>("AsyncConfig");
+
 @Component({
   selector: "app-root",
   standalone: true,
-  providers: [SyncService],
+  imports: [JsonPipe],
+  providers: [
+    SyncService,
+    {
+      provide: AsyncConfig,
+      useFactory: async () => {
+        const config = await fetch("/config.json").then((r) => r.json());
+        await new Promise((r) => setTimeout(r, 5_000));
+        return config;
+      },
+    },
+  ],
   template: `
     <h1>Hello from {{ name }}!</h1>
     <p>appName = {{ appName }}</p>
+    <p>dynamicUrl = {{ asyncConfig | json }}</p>
   `,
 })
 export class App {
   name = "Angular";
   appName = inject(SyncService).config.appName;
+  asyncConfig = inject(AsyncConfig);
 }
 
 bootstrapApplication(App);
