ca995bfe4882a422c3b934bf1c4f5619e8c7a966
Anthony Pena <anthony.pena@outlook.fr>
2024-11-13 23:23:31 +0100

    add sync service

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
-import { bootstrapApplication } from '@angular/platform-browser';
+import { Component, inject, Injectable } from "@angular/core";
+import { bootstrapApplication } from "@angular/platform-browser";
+
+@Injectable()
+export class SyncService {
+  config = { appName: "Async provider demo" };
+}
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
+  providers: [SyncService],
   template: `
     <h1>Hello from {{ name }}!</h1>
+    <p>appName = {{ appName }}</p>
   `,
 })
 export class App {
-  name = 'Angular';
+  name = "Angular";
+  appName = inject(SyncService).config.appName;
 }
 
 bootstrapApplication(App);
