ff2a513b3e90a10707d4c1f51e54b1b703252eac
kuroidoruido <anthony.pena@outlook.fr>
2024-11-13 23:06:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-async-provider
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/kuroidoruido/angular-async-provider)
---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  template: `
+    <h1>Hello from {{ name }}!</h1>
+  `,
+})
+export class App {
+  name = 'Angular';
+}
+
+bootstrapApplication(App);
