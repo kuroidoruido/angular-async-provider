10c26a792b4cb497de3f63c15020bbcbd1240dc7
Anthony Pena <anthony.pena@outlook.fr>
2024-11-14 00:18:52 +0100

    inplace async provider

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
-import { AsyncPipe, JsonPipe } from "@angular/common";
-import { HttpClient, provideHttpClient } from "@angular/common/http";
+import { JsonPipe } from "@angular/common";
 import { Component, inject, Injectable } from "@angular/core";
 import { bootstrapApplication } from "@angular/platform-browser";
 
@@ -12,26 +11,32 @@ interface AsyncConfig {
   url: string;
 }
 
-@Injectable()
 export class AsyncConfigService {
-  config$ = inject(HttpClient).get<AsyncConfig>("/config.json");
+  constructor(public readonly config: AsyncConfig) {}
 }
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [JsonPipe, AsyncPipe],
-  providers: [SyncService, AsyncConfigService],
+  imports: [JsonPipe],
+  providers: [SyncService],
   template: `
     <h1>Hello from {{ name }}!</h1>
     <p>appName = {{ appName }}</p>
-    <p>dynamicUrl = {{ asyncConfig | async | json }}</p>
+    <p>dynamicUrl = {{ asyncConfig | json }}</p>
   `,
 })
 export class App {
   name = "Angular";
   appName = inject(SyncService).config.appName;
-  asyncConfig = inject(AsyncConfigService).config$;
+  asyncConfig = inject(AsyncConfigService).config;
 }
 
-bootstrapApplication(App, { providers: [provideHttpClient()] });
+(async () => {
+  const config = await fetch("/config.json").then((r) => r.json());
+  bootstrapApplication(App, {
+    providers: [
+      { provide: AsyncConfigService, useValue: new AsyncConfigService(config) },
+    ],
+  });
+})();
